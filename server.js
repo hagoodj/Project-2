173a6c6fd7d5016467341f65382e03618aaf1020
Andrew Duong <duongsters@gmail.com>
2019-10-11 13:36:23 -0700

    static content for the app from the public directory in the application directory.

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ var app = express();
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
