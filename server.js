f02ada019232f196cd63bd60d5afc2d8384e9b0f
Andrew Duong <duongsters@gmail.com>
2019-10-11 12:57:00 -0700

    Set the port of our application

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ var express = require("express");
 
 // Create express app instance.
 var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
