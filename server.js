f73d5657f2c087eeae10b9b7494ea000b0cafb6c
Andrew Duong <duongsters@gmail.com>
2019-10-11 13:46:05 -0700

    finished server.js

--- server.js
+++ server.js
@@ -15,3 +15,15 @@ app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
