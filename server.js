33f1f9c5fd37fa5bb59c2edcb65f302fbe036c41
Andrew Duong <duongsters@gmail.com>
2019-10-14 12:33:53 -0700

    updated server.js and sequalized it by remving all orm calls

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Requiring our models for syncing
+var db = require("./models");
+
 
 
 // Parse application body as JSON
@@ -17,13 +18,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-app.use(routes);
+
+//ROUTES
+// Import routes and give the server access to them.
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
