a1ceb88e47357e094ef3a2b643e194ec5f1fe9ab
jordan hagood <jordan.hagood@yahoo.com>
2019-10-15 13:12:33 -0700

    updated api routes

--- routes/movie-api-routes.js
+++ routes/movie-api-routes.js
@@ -4,35 +4,28 @@ module.exports = function (app) {
 
   app.get("/api/movies", function (req, res) {
 
-    db.Movie.findAll({}).then(function (results) {
-      res.json(results);
+    db.Movie.findAll({}).then(function (dbMovie) {
+      res.json(dbMovie);
     });
 
   });
 
-  app.get("/api/:username/:userpin/movies", function (req, res) {
+  app.post("/api/movies", function (req, res) {
 
-    db.userMovie.findAll({
-      where: {
-        username: req.params.username,
-        userpin: req.params.userpin
-      }
-    }).then(function (results) {
-      res.json(results);
-    });
-
-  });
-
-  app.post("/api/newmovie", function (req, res) {
     db.Movie.create(req.body).then(function (dbMovie) {
       res.json(dbMovie);
     });
+
   });
 
-  app.post("/api/newusermovie", function (req, res) {
-    db.userMovie.create(req.body).then(function (dbuserMovie) {
-      res.json(dbuserMovie);
-    });
-  });
+  app.get("api/:usermovieid/movies", function(req,res) {
+
+    db.userMovie.findAll({
+      where: {
+        UserId: req.params.usermovieid
+      }
+    })
+    
+  })
 
 }
